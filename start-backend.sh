e439c8d995f5b46895613b3270f9165dd627c5b1
Devendr Mishra <devendra.mishra@adypu.edu.in>
2025-11-22 15:23:04 +0530

    Delete start-backend.sh

--- start-backend.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "🚀 Starting Success Mantra Backend..."
-echo "📡 Backend will run on http://localhost:5001"
-echo "☁️  Using MongoDB Atlas Cloud Database"
-echo ""
-
-cd backend
-npm run dev
