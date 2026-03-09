35acb565783bf934acd402fc607c653caacc08bc
Devendr Mishra <devendramishra495@gmail.com>
2026-03-09 18:40:47 +0530

    Update cloudinary.js

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -4,11 +4,6 @@ const cloudName = process.env.CLOUDINARY_CLOUD_NAME;
 const apiKey = process.env.CLOUDINARY_API_KEY;
 const apiSecret = process.env.CLOUDINARY_API_SECRET;
 
-console.log('Cloudinary Config Check:');
-console.log('CLOUD_NAME:', cloudName);
-console.log('API_KEY:', apiKey);
-console.log('API_SECRET:', apiSecret ? 'Set' : 'Not Set');
-
 cloudinary.config({
   cloud_name: cloudName,
   api_key: apiKey,
